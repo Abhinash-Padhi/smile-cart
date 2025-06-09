9567529dc147c0bf6b5d429094408ac3def5ee1f
Abhinash-Padhi <abhinashpadhi79@gmail.com>
2025-06-10 00:16:46 +0530

    Initialized i18n

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { ToastContainer } from "react-toastify";
 import ReactDOM from "react-dom/client";
-
+import "./common/i18n";
 import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
