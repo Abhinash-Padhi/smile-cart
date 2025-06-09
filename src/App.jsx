24038a6cad9ea506fd08c2b51f1082e3662ab433
Abhinash-Padhi <abhinashpadhi79@gmail.com>
2025-06-10 01:42:07 +0530

    Integrated Formik form into the checkout page

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,21 @@
-import "./App.css";
-// eslint-disable-next-line import/extensions
-
-
-import { NavLink, Route, Switch, Redirect } from "react-router-dom";
-import routes from "routes";
-import ProductList from "./components/ProductList";
-import Product from "./components/Product";
 import { PageNotFound } from "components/commons";
-import Cart from "components/Cart";
+import { Route, Switch, Redirect } from "react-router-dom";
+import routes from "routes";
 
-const App = () => {
-    // const [cartItems, setCartItems] = useState([]);
-    return (
-        <>
-            <Switch>
-                <Route exact component={Product} path={routes.products.show} />
-                <Route exact component={ProductList} path={routes.products.index} />
-                <Route exact component={Cart} path={routes.cart} />
-                <Redirect exact from={routes.root} to={routes.products.index} />
-                <Route component={PageNotFound} path="*" />
-            </Switch>
-        </>
-    );
-}
+import "./App.css";
+import Cart from "./components/Cart";
+import Checkout from "./components/Checkout";
+import Product from "./components/Product";
+import ProductList from "./components/ProductList";
 
+const App = () => (
+    <Switch>
+        <Route exact component={Cart} path={routes.cart} />
+        <Route exact component={Checkout} path={routes.checkout} />
+        <Route exact component={Product} path={routes.products.show} />
+        <Route exact component={ProductList} path={routes.products.index} />
+        <Redirect exact from={routes.root} to={routes.products.index} />
+        <Route component={PageNotFound} path="*" />
+    </Switch>
+);
 export default App;
