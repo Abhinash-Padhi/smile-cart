166300ae2c6f0e484f355b8bbecc064216d88ceb
Abhinash-Padhi <abhinashpadhi79@gmail.com>
2025-06-10 10:16:32 +0530

    Enhanced app performance using memoization

--- src/components/Cart/PriceCard.jsx
+++ src/components/Cart/PriceCard.jsx
@@ -1,12 +1,15 @@
+import { memo } from "react";
+
 import classNames from "classnames";
-import { Button, Typography } from "neetoui";
+import { Typography, Button } from "neetoui";
 import { gt, keys } from "ramda";
+import { useTranslation, Trans } from "react-i18next";
 import routes from "routes";
 import useCartItemsStore from "stores/useCartItemsStore";
-import { useTranslation, Trans } from "react-i18next";
 
 const PriceCard = ({ totalMrp, totalOfferPrice }) => {
     const { t } = useTranslation();
+
     const totalDiscounts = totalMrp - totalOfferPrice;
     const isDiscountPresent = gt(totalDiscounts, 0);
     const discountPercentage = ((totalDiscounts / totalMrp) * 100).toFixed(1);
@@ -26,7 +29,6 @@ const PriceCard = ({ totalMrp, totalOfferPrice }) => {
                     values={{ mrp: totalMrp }}
                 />
             </Typography>
-
             {isDiscountPresent && (
                 <>
                     <Typography className="flex justify-between text-green-700">
@@ -36,21 +38,18 @@ const PriceCard = ({ totalMrp, totalOfferPrice }) => {
                             values={{ discounts: totalDiscounts, discountPercentage }}
                         />
                     </Typography>
-
                     <Typography className="flex justify-between">
                         <Trans
                             components={{ span: <span /> }}
-                            i18nKey="offerPrice"
+                            i18nKey="totalOfferPrice"
                             values={{ offerPrice: totalOfferPrice }}
                         />
                     </Typography>
-
                     <span className="neeto-ui-text-gray-500 text-sm">
                         {t("itemCount", { count: itemsCount })}
                     </span>
                 </>
             )}
-
             <div className="flex flex-col items-center pt-4">
                 <Button
                     className="bg-neutral-800"
@@ -61,5 +60,4 @@ const PriceCard = ({ totalMrp, totalOfferPrice }) => {
         </div>
     );
 };
-
-export default PriceCard;
+export default memo(PriceCard);
--- src/components/commons/Header.jsx
+++ src/components/commons/Header.jsx
@@ -1,17 +1,19 @@
+import { memo } from "react";
+
 import { LeftArrow } from "neetoicons";
 import { Typography } from "neetoui";
-import { useHistory } from "react-router-dom";
-import { AiOutlineShoppingCart } from "react-icons/ai";
-import useCartItemsStore from "stores/useCartItemsStore";
 import { keys } from "ramda";
-import { Link } from "react-router-dom";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { Link, useHistory } from "react-router-dom";
 import routes from "routes";
+import useCartItemsStore from "stores/useCartItemsStore";
 
-const Header = ({ title, shouldShowBackButton = true, actionBlock, }) => {
+const Header = ({ title, shouldShowBackButton = true, actionBlock }) => {
+    const history = useHistory();
+
     const cartItemsCount = useCartItemsStore(
         store => keys(store.cartItems).length
     );
-    const history = useHistory();
 
     return (
         <div className="m-2">
@@ -27,7 +29,8 @@ const Header = ({ title, shouldShowBackButton = true, actionBlock, }) => {
                         {title}
                     </Typography>
                 </div>
-                <div className="flex items-end space-x-4">{actionBlock}
+                <div className="flex items-end space-x-4">
+                    {actionBlock}
                     <div className="flex flex-col">
                         {cartItemsCount > 0 && (
                             <span className="neeto-ui-border-black neeto-ui-rounded-full min-w-fit flex h-5 w-5 items-center self-end border p-1">
@@ -45,4 +48,4 @@ const Header = ({ title, shouldShowBackButton = true, actionBlock, }) => {
     );
 };
 
-export default Header;
+export default memo(Header);
